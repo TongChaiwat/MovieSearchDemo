f8acad715a990a636760afd6a8bc723b03343dd3
Chaiwat Luecha <chaiwat.lue@mtel.co.th>
2020-02-26 11:12:58 +0700

    change context toast.

--- app/src/main/java/com/charmist/moviesearchdemo/utils/extension/Toast.kt
+++ app/src/main/java/com/charmist/moviesearchdemo/utils/extension/Toast.kt
@@ -3,5 +3,5 @@ import android.app.Activity
 import android.widget.Toast
 
 fun Activity.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, message, length).show()
+    Toast.makeText(applicationContext, message, length).show()
 }
